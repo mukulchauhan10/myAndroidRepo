01ce6da30e439bf79256e0298e4b21bcea685f01
Mukul Chauhan <mukulchauhan10@gmail.com>
2020-03-26 16:54:26 +0530

    adding broadcast: aeroplane mode changed, for whole phone

---
+++ Broadcasting Service/app/src/main/java/com/example/broadcastingservice/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.broadcastingservice
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    val receiver = MyBroadcastReceiver()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+}
---
+++ Broadcasting Service/app/src/main/java/com/example/broadcastingservice/MyBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.broadcastingservice
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class MyBroadcastReceiver: BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.i("receivedBroadcast", " mode changed")
+    }
+
+}
